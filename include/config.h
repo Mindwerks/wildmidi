15af1df2586ed885a9bcd90afedc4d7104754e4d
Bret Curtis <psi29a@gmail.com>
2014-01-12 09:05:48 +0100

    trimming up config.h

--- include/config.h
+++ include/config.h
@@ -1,94 +1,4 @@
-/* include/config.h.  Generated from config.h.in by configure.  */
-
-/* Define if building universal (internal helper macro) */
-/* #undef AC_APPLE_UNIVERSAL_BUILD */
-
-/* Define this if using Alsa 1.0.1 or higher */
-/* #undef ALSA_NEW */
-
-/* Define this if your system uses alsa */
-/* #undef HAVE_ALSA */
-
-/* Define to 1 if you have the <alsa/asoundlib.h> header file. */
-/* #undef HAVE_ALSA_ASOUNDLIB_H */
-
-/* Define to 1 if you have the <ctype.h> header file. */
-#define HAVE_CTYPE_H 1
-
-/* Define to 1 if you have the <dlfcn.h> header file. */
-#define HAVE_DLFCN_H 1
-
-/* Define to 1 if you have the <errno.h> header file. */
-#define HAVE_ERRNO_H 1
-
-/* Define to 1 if you have the <fcntl.h> header file. */
-#define HAVE_FCNTL_H 1
-
-/* Define to 1 if you have the <getopt.h> header file. */
-#define HAVE_GETOPT_H 1
-
-/* Define to 1 if you have the <inttypes.h> header file. */
-#define HAVE_INTTYPES_H 1
-
-/* Define to 1 if you have the <linux/soundcard.h> header file. */
-/* #undef HAVE_LINUX_SOUNDCARD_H */
-
-/* Define to 1 if you have the <machine/soundcard.h> header file. */
-/* #undef HAVE_MACHINE_SOUNDCARD_H */
-
-/* Define to 1 if you have the <math.h> header file. */
-#define HAVE_MATH_H 1
-
-/* Define to 1 if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
-
-/* Define to 1 if you have the <mmsystem.h> header file. */
-/* #undef HAVE_MMSYSTEM_H */
-
-/* Define to 1 if you have the <pwd.h> header file. */
-/* #undef HAVE_PWD_H */
-
-/* Define to 1 if you have the <stdarg.h> header file. */
-#define HAVE_STDARG_H 1
-
-/* Define to 1 if you have the <stdint.h> header file. */
-#define HAVE_STDINT_H 1
-
-/* Define to 1 if you have the <stdio.h> header file. */
-#define HAVE_STDIO_H 1
-
-/* Define to 1 if you have the <stdlib.h> header file. */
-#define HAVE_STDLIB_H 1
-
-/* Define to 1 if you have the <strings.h> header file. */
-#define HAVE_STRINGS_H 1
-
-/* Define to 1 if you have the <string.h> header file. */
-#define HAVE_STRING_H 1
-
-/* Define to 1 if you have the <sys/ioctl.h> header file. */
-/* #undef HAVE_SYS_IOCTL_H */
-
-/* Define to 1 if you have the <sys/mman.h> header file. */
-/* #undef HAVE_SYS_MMAN_H */
-
-/* Define to 1 if you have the <sys/soundcard.h> header file. */
-/* #undef HAVE_SYS_SOUNDCARD_H */
-
-/* Define to 1 if you have the <sys/stat.h> header file. */
-#define HAVE_SYS_STAT_H 1
-
-/* Define to 1 if you have the <sys/types.h> header file. */
-#define HAVE_SYS_TYPES_H 1
-
-/* Define to 1 if you have the <termios.h> header file. */
-/* #undef HAVE_TERMIOS_H */
-
-/* Define to 1 if you have the <unistd.h> header file. */
-#define HAVE_UNISTD_H 1
-
-/* Define to 1 if you have the <windows.h> header file. */
-#define HAVE_WINDOWS_H 1
+/* include/config.h.  Generated from config.h.cmake by configure.  */
 
 /* Define this if the GCC __builtin_expect keyword is available */
 #define HAVE___BUILTIN_EXPECT 1
@@ -98,13 +8,12 @@
 
 /* Define to the sub-directory in which libtool stores uninstalled libraries.
    */
-#define LT_OBJDIR ".libs/"
 
 /* Name of package */
 #define PACKAGE "wildmidi"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "http://sourceforge.net/tracker/?group_id=42635&atid=433744"
+#define PACKAGE_BUGREPORT "https://github.com/psi29a/wildmidi/issues"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "WildMidi"
@@ -116,13 +25,10 @@
 #define PACKAGE_TARNAME "wildmidi"
 
 /* Define to the home page for this package. */
-#define PACKAGE_URL "http://wildmidi.sourceforge.net"
+#define PACKAGE_URL "https://github.com/psi29a/wildmidi"
 
 /* Define to the version of this package. */
 #define PACKAGE_VERSION "0.2.3.5"
-
-/* Define to 1 if you have the ANSI C header files. */
-#define STDC_HEADERS 1
 
 /* Version number of package */
 #define VERSION "0.2.3.5"
