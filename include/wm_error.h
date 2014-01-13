eba9fd09a11e4275230951106ebd763c23d23b46
Bret Curtis <psi29a@gmail.com>
2014-01-13 15:39:05 +0100

    format fixups and some fprint magic

--- include/wm_error.h
+++ include/wm_error.h
@@ -1,28 +1,28 @@
 /*
-	wm_error.h
+ wm_error.h
 
- 	error reporting
+ error reporting
 
-    Copyright (C) Chris Ison 2001-2011
-    Copyright (C) Bret Curtis 2013-2014
+ Copyright (C) Chris Ison 2001-2011
+ Copyright (C) Bret Curtis 2013-2014
 
-    This file is part of WildMIDI.
+ This file is part of WildMIDI.
 
-    WildMIDI is free software: you can redistribute and/or modify the player
-    under the terms of the GNU General Public License and you can redistribute
-    and/or modify the library under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation, either version 3 of
-    the licenses, or(at your option) any later version.
+ WildMIDI is free software: you can redistribute and/or modify the player
+ under the terms of the GNU General Public License and you can redistribute
+ and/or modify the library under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation, either version 3 of
+ the licenses, or(at your option) any later version.
 
-    WildMIDI is distributed in the hope that it will be useful, but WITHOUT
-    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-    FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License and
-    the GNU Lesser General Public License for more details.
+ WildMIDI is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License and
+ the GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU General Public License and the
-    GNU Lesser General Public License along with WildMIDI.  If not,  see
-    <http://www.gnu.org/licenses/>.
-*/
+ You should have received a copy of the GNU General Public License and the
+ GNU Lesser General Public License along with WildMIDI.  If not,  see
+ <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef __WM_ERROR_H
 #define __WM_ERROR_H
@@ -38,7 +38,12 @@
 #define WM_ERR_INVALID_ARG	8
 #define WM_ERR_ALR_INIT     9
 
-extern void WM_ERROR_NEW (const char * wmfmt, ...);
-extern void WM_ERROR( const char * func, unsigned long int lne, int wmerno, const char * wmfor, int error);
+extern void WM_ERROR_NEW(const char * wmfmt, ...)
+#ifdef __GNUC__
+		__attribute__((format(printf, 1, 2))) // for tracking down formatting issues
+#endif
+		;
+extern void WM_ERROR(const char * func, unsigned long int lne, int wmerno,
+		const char * wmfor, int error);
 
 #endif //__WM_ERROR_H
