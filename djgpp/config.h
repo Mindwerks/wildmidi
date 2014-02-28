eab97b4fb4db9b04904f6aa5d504dfca45c2d470
sezero <sezero@users.noreply.github.com>
2014-02-28 12:55:01 +0200

    update djgpp config.h

--- djgpp/config.h
+++ djgpp/config.h
@@ -1,14 +1,12 @@
-#define VERSION "0.4.0"
 #define WILDMIDI_CFG "wildmidi.cfg"
 
 #define PACKAGE_URL "http://www.mindwerks.net/projects/wildmidi/"
 #define PACKAGE_BUGREPORT "https://github.com/Mindwerks/wildmidi/issues"
 
-#define PACKAGE_VERSION VERSION
+#define PACKAGE_VERSION "0.4.0"
 
 #define HAVE_C_INLINE
 
 #if (__GNUC__ > 2) || (__GNUC__ == 2 && __GNUC_MINOR >= 96)
 #define HAVE___BUILTIN_EXPECT
 #endif
-
