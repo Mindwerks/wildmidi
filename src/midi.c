57a91d4e40a0dff320caba7b6e09a9b820b84efc
chrisisonwildcode <chrisisonwildcode@gmail.com>
2014-05-28 22:58:31 +1000

    Fix sysex message recording to midi file output.

--- src/midi.c
+++ src/midi.c
@@ -768,7 +768,7 @@ _WM_Event2Midi(struct _mdi *mdi, uint8_t **out, uint32_t *outsize) {
         } else if (mdi->events[i].do_event == _WM_do_sysex_roland_drum_track) {
             // DEBUG
             // fprintf(stderr,"Sysex Roland Drum Track: %u %.4x\r\n",mdi->events[i].event_data.channel, mdi->events[i].event_data.data);
-            int8_t foo[] = {0xf0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x15, 0x00, 0xf7};
+            int8_t foo[] = {0xf0, 0x09, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x15, 0x00, 0xf7};
             uint8_t foo_ch = mdi->events[i].event_data.channel;
             if (foo_ch == 9) {
                 foo_ch = 0;
@@ -778,28 +778,28 @@ _WM_Event2Midi(struct _mdi *mdi, uint8_t **out, uint32_t *outsize) {
             foo[7] = 0x10 | foo_ch;
             foo[9] = mdi->events[i].event_data.data;
             memcpy(&((*out)[out_ofs]),foo,10);
-            out_ofs += 10;
+            out_ofs += 11;
             running_event = 0;
         }  else if (mdi->events[i].do_event == _WM_do_sysex_gm_reset) {
             // DEBUG
             // fprintf(stderr,"Sysex GM Reset\r\n");
-            int8_t foo[] = {0xf0, 0x7e, 0x7f, 0x09, 0x01, 0xf7};
+            int8_t foo[] = {0xf0, 0x05, 0x7e, 0x7f, 0x09, 0x01, 0xf7};
             memcpy(&((*out)[out_ofs]),foo,6);
-            out_ofs += 6;
+            out_ofs += 7;
             running_event = 0;
         } else if (mdi->events[i].do_event == _WM_do_sysex_roland_reset) {
             // DEBUG
             // fprintf(stderr,"Sysex Roland Reset\r\n");
-            int8_t foo[] = {0xf0, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7f, 0x00, 0x41, 0xf7};
+            int8_t foo[] = {0xf0, 0x0a, 0x41, 0x10, 0x42, 0x12, 0x40, 0x00, 0x7f, 0x00, 0x41, 0xf7};
             memcpy(&((*out)[out_ofs]),foo,11);
-            out_ofs += 11;
+            out_ofs += 12;
             running_event = 0;
         }  else if (mdi->events[i].do_event == _WM_do_sysex_yamaha_reset) {
             // DEBUG
             // fprintf(stderr,"Sysex Yamaha Reset\r\n");
-            int8_t foo[] = {0xf0, 0x43, 0x10, 0x4c, 0x00, 0x00, 0x7e, 0x00, 0xf7};
+            int8_t foo[] = {0xf0, 0x08, 0x43, 0x10, 0x4c, 0x00, 0x00, 0x7e, 0x00, 0xf7};
             memcpy(&((*out)[out_ofs]),foo,9);
-            out_ofs += 9;
+            out_ofs += 10;
             running_event = 0;
         } else if (mdi->events[i].do_event == _WM_do_meta_endoftrack) {
             // DEBUG
