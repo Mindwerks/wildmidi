a162e9a21d64a2ed5bd4ad04be4123f4eb6b8f25
sezero <sezero@users.noreply.github.com>
2016-07-07 12:02:01 +0300

    add amiga makefiles under a new amiga directory

---
+++ amiga/config.h
@@ -0,0 +1,19 @@
+/* wildmidi config for amigaos variants */
+#define WILDMIDI_AMIGA 1
+
+#define WILDMIDI_CFG "wildmidi.cfg"
+
+#define PACKAGE_URL "http://www.mindwerks.net/projects/wildmidi/"
+#define PACKAGE_BUGREPORT "https://github.com/Mindwerks/wildmidi/issues"
+
+#define PACKAGE_VERSION "0.4.1"
+
+#define HAVE_C_INLINE
+
+#if defined(__GNUC__) && ((__GNUC__ > 2) || (__GNUC__ == 2 && __GNUC_MINOR >= 96))
+#define HAVE___BUILTIN_EXPECT
+#endif
+#ifndef HAVE___BUILTIN_EXPECT
+#define __builtin_expect(x,c) x
+#endif
+
