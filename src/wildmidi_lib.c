fcf74ab06138ede7959f3aaecbe48d94e5050bbd
sezero <sezero@users.noreply.github.com>
2016-07-08 19:24:35 +0300

    WildMidi_GetMidiOutput() should respect the returned code from _WM_Event2Midi()

--- src/wildmidi_lib.c
+++ src/wildmidi_lib.c
@@ -1935,8 +1935,7 @@ WM_SYMBOL int WildMidi_GetMidiOutput(midi * handle, int8_t **buffer, uint32_t *s
         _WM_GLOBAL_ERROR(__FUNCTION__, __FILE__, __LINE__, WM_ERR_INVALID_ARG, "(NULL buffer pointer)", 0);
         return (-1);
     }
-    _WM_Event2Midi(handle, (uint8_t **)buffer, size);
-    return 0;
+    return _WM_Event2Midi(handle, (uint8_t **)buffer, size);
 }
 
 
