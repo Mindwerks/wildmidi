43dc4d5bac8f3a1a1555ff1244dcf35d4ed1c6a3
loki666 <simons.philippe@gmail.com>
2016-08-18 12:01:11 +0200

    android: change DSO filename to libWildMidi.so

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_ABI               := armeabi-v7a x86
-APP_MODULES           := wildmidi
+APP_MODULES           := WildMidi
 APP_PLATFORM          := android-10
 APP_OPTIM             := release
 NDK_TOOLCHAIN_VERSION := 4.9
