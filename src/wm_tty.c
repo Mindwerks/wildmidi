cec16eff651eeef83c7b4e1484f6f97cc0aa059e
Bret Curtis <psi29a@gmail.com>
2017-07-10 15:24:38 +0200

    add *BSD fix for ONLCR support

--- src/wm_tty.c
+++ src/wm_tty.c
@@ -27,6 +27,7 @@
 #if !(defined(_WIN32) || defined(__DJGPP__) || defined(WILDMIDI_AMIGA) || defined(__OS2__) || defined(__EMX__))
 
 #define _XOPEN_SOURCE 600 /* for ONLCR */
+#define __BSD_VISIBLE 1 /* for ONLCR in *BSD */
 
 #include <unistd.h>
 #include <termios.h>
