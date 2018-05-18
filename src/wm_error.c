fd4f18e417a97dfba68c2507a80204eb4704f21a
sezero <sezero@users.sourceforge.net>
2018-05-18 14:01:10 +0300

    adjust WM_ERR_MEM string

--- src/wm_error.c
+++ src/wm_error.c
@@ -1,26 +1,25 @@
 /*
- wm_error.c
- error reporting
-
- Copyright (C) Chris Ison  2001-2011
- Copyright (C) Bret Curtis 2013-2016
-
- This file is part of WildMIDI.
-
- WildMIDI is free software: you can redistribute and/or modify the player
- under the terms of the GNU General Public License and you can redistribute
- and/or modify the library under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation, either version 3 of
- the licenses, or(at your option) any later version.
-
- WildMIDI is distributed in the hope that it will be useful, but WITHOUT
- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License and
- the GNU Lesser General Public License for more details.
-
- You should have received a copy of the GNU General Public License and the
- GNU Lesser General Public License along with WildMIDI.  If not,  see
- <http://www.gnu.org/licenses/>.
+ * wm_error.c -- error reporting
+ *
+ * Copyright (C) Chris Ison  2001-2011
+ * Copyright (C) Bret Curtis 2013-2016
+ *
+ * This file is part of WildMIDI.
+ *
+ * WildMIDI is free software: you can redistribute and/or modify the player
+ * under the terms of the GNU General Public License and you can redistribute
+ * and/or modify the library under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either version 3 of
+ * the licenses, or(at your option) any later version.
+ *
+ * WildMIDI is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License and
+ * the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License and the
+ * GNU Lesser General Public License along with WildMIDI.  If not,  see
+ * <http://www.gnu.org/licenses/>.
  */
 
 #include "config.h"
@@ -39,27 +38,27 @@ void _WM_ERROR_NEW(const char * wmfmt, ...) {
 	fprintf(stderr, "\n");
 }
 
+static const char *errors[WM_ERR_MAX+1] = {
+	"No error",
+
+	"Unable to allocate memory",
+	"Unable to stat",
+	"Unable to load",
+	"Unable to open",
+	"Unable to read",
+	"Invalid or Unsuported file format",
+	"File corrupt",
+	"Library not Initialized",
+	"Invalid argument",
+	"Library Already Initialized",
+	"Not a midi file",
+	"Refusing to load unusually long file",
+
+	"Invalid error code"
+};
+
 void _WM_ERROR(const char *func, int lne, int wmerno,
 		const char *wmfor, int error) {
-
-	static const char *errors[WM_ERR_MAX+1] = {
-		"No error",
-
-		"Unable to obtain memory",
-		"Unable to stat",
-		"Unable to load",
-		"Unable to open",
-		"Unable to read",
-		"Invalid or Unsuported file format",
-		"File corrupt",
-		"Library not Initialized",
-		"Invalid argument",
-		"Library Already Initialized",
-		"Not a midi file",
-		"Refusing to load unusually long file",
-
-		"Invalid error code"
-	};
 
 	if (wmerno < 0 || wmerno > WM_ERR_MAX)
 		wmerno = WM_ERR_MAX;
