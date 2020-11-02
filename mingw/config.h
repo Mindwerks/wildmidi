0ac05f58e5a0d35826bc11367d61ff85fcc7ce64
Ozkan Sezer <sezeroz@gmail.com>
2020-11-02 06:30:20 +0300

    added standalone config/makefile for MinGW to make my life easier.

---
+++ mingw/config.h
@@ -0,0 +1,18 @@
+#define WILDMIDI_CFG "wildmidi.cfg"
+
+#define PACKAGE_URL "http://www.mindwerks.net/projects/wildmidi/"
+#define PACKAGE_BUGREPORT "https://github.com/Mindwerks/wildmidi/issues"
+
+#define PACKAGE_VERSION "0.4.4"
+
+#define HAVE_C_INLINE
+
+#if (__GNUC__ > 2) || (__GNUC__ == 2 && __GNUC_MINOR >= 96)
+#define HAVE___BUILTIN_EXPECT
+#endif
+#ifndef HAVE___BUILTIN_EXPECT
+#define __builtin_expect(x,c) x
+#endif
+
+#define HAVE_STDINT_H 1
+#define HAVE_INTTYPES_H 1
