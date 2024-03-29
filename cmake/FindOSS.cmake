095048a0047284bb2673677ecdf5773a61e2fa89
Ozkan Sezer <sezeroz@gmail.com>
2024-03-29 14:10:28 +0300

    cmake: update FindOSS module so that it responds to 'REQUIRED' flag

--- cmake/FindOSS.cmake
+++ cmake/FindOSS.cmake
@@ -1,56 +1,34 @@
-# - Find OSS
-# Find OSS headers and libraries.
+# - Find OSS includes
 #
-#  OSS_INCLUDE_DIR  - where to find soundcard.h, etc.
-#  OSS_LIBRARY      - link library, if any, needed for OSS.
-#  OSS_FOUND        - True if OSS found.
+#   OSS_FOUND        - True if OSS_INCLUDE_DIR is found
+#   OSS_INCLUDE_DIRS - Set when OSS_INCLUDE_DIR is found
+#   OSS_LIBRARIES    - Set when OSS_LIBRARY is found
+#
+#   OSS_INCLUDE_DIR  - where to find sys/soundcard.h
+#   OSS_LIBRARY      - where to find libossaudio (for NetBSD)
+#
+message(STATUS "Looking for OSS...")
 
-INCLUDE(CheckIncludeFiles)
-INCLUDE(CheckCSourceCompiles)
+include(FindPackageHandleStandardArgs)
 
-SET(OSS_LIBRARY "")
-SET(OSS_INCLUDE_DIR) # system header must suffice
-SET(OSS_FOUND)
+find_path(OSS_INCLUDE_DIR NAMES sys/soundcard.h)
 
-MESSAGE(STATUS "Looking for OSS...")
+# NetBSD uses ossaudio emulation layer:
+if(CMAKE_SYSTEM_NAME MATCHES "kNetBSD.*|NetBSD.*")
+  find_library(OSS_LIBRARY NAMES ossaudio)
+  find_package_handle_standard_args(OSS REQUIRED_VARS OSS_INCLUDE_DIR OSS_LIBRARY)
+else()
+  unset(OSS_LIBRARY)
+  find_package_handle_standard_args(OSS REQUIRED_VARS OSS_INCLUDE_DIR)
+endif()
 
-CHECK_INCLUDE_FILES(sys/soundcard.h HAVE_SYS_SOUNDCARD_H)
+if(OSS_FOUND)
+  set(OSS_INCLUDE_DIRS ${OSS_INCLUDE_DIR})
+  if(OSS_LIBRARY)
+    set(OSS_LIBRARIES ${OSS_LIBRARY})
+  else()
+    unset(OSS_LIBRARIES)
+  endif()
+endif()
 
-# NetBSD uses ossaudio emulation layer,
-# otherwise no link library is needed.
-IF(CMAKE_SYSTEM_NAME MATCHES "kNetBSD.*|NetBSD.*")  # AND HAVE_SOUNDCARD_H ???
-  FIND_LIBRARY(OSSAUDIO_LIBRARIES "ossaudio")
-  IF(OSSAUDIO_LIBRARIES STREQUAL "OSSAUDIO_LIBRARIES-NOTFOUND")
-    SET(OSSAUDIO_LIBRARIES)
-  ELSE()
-    MESSAGE(STATUS "Found libossaudio: ${OSSAUDIO_LIBRARIES}")
-    SET(OSS_LIBRARY ${OSSAUDIO_LIBRARIES})
-  ENDIF()
-ELSE()
-  SET(OSSAUDIO_LIBRARIES)
-ENDIF()
-
-SET(OLD_REQUIRED_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES}")
-IF(OSSAUDIO_LIBRARIES)
-  SET(CMAKE_REQUIRED_LIBRARIES ${OSSAUDIO_LIBRARIES})
-ENDIF()
-
-IF(HAVE_SYS_SOUNDCARD_H)
-    CHECK_C_SOURCE_COMPILES("#include <sys/ioctl.h>
-                             #include <sys/soundcard.h>
-                             int main() {return SNDCTL_DSP_RESET;}" OSS_FOUND)
-ENDIF()
-
-SET(CMAKE_REQUIRED_LIBRARIES "${OLD_REQUIRED_LIBRARIES}")
-
-MARK_AS_ADVANCED (
-	OSS_FOUND
-	OSS_INCLUDE_DIR
-	OSS_LIBRARY
-)
-
-IF(OSS_FOUND)
-    MESSAGE(STATUS "Found OSS.")
-ELSE()
-    MESSAGE(STATUS "Could not find OSS.")
-ENDIF()
+mark_as_advanced(OSS_INCLUDE_DIR OSS_LIBRARY)
