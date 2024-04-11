175402ea2e65fc17a1d94e26c86a6a05deac01b6
Ozkan Sezer <sezeroz@gmail.com>
2024-04-11 03:11:10 +0300

    minor update to android makefile

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,5 +1,5 @@
-APP_ABI               := armeabi-v7a x86
+APP_ABI               := all
 APP_MODULES           := WildMidi
 APP_PLATFORM          := android-10
 APP_OPTIM             := release
-NDK_TOOLCHAIN_VERSION := 4.9
+#NDK_TOOLCHAIN_VERSION := 4.9
