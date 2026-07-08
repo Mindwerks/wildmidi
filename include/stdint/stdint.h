21dbbf815bb68b2b063ba2c1a7c679e123a86568
Ozkan Sezer <sezeroz@gmail.com>
2026-07-08 23:35:40 +0300

    define MIN / MAX macros in our replacement stdint.h

--- include/stdint/stdint.h
+++ include/stdint/stdint.h
@@ -8,12 +8,23 @@ typedef unsigned char uint8_t;
 typedef signed short int int16_t;
 typedef unsigned short int uint16_t;
 
+#define INT8_MIN (-128)
+#define INT16_MIN (-32767-1)
+#define INT32_MIN (-2147483647-1)
+#define INT8_MAX (127)
+#define INT16_MAX (32767)
+#define INT32_MAX (2147483647)
+#define UINT8_MAX (255)
+#define UINT16_MAX (65535)
+
 #if (INT_MAX == 2147483647)
 typedef signed int int32_t;
 typedef unsigned int uint32_t;
+#define UINT32_MAX (4294967295U)
 #elif (LONG_MAX == 2147483647)
 typedef signed long int int32_t;
 typedef unsigned long int uint32_t;
+#define UINT32_MAX (4294967295UL)
 #else
 #error define a 32bit integral type
 #endif /* int32_t */
